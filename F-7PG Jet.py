041b07e2bc56ffeebc6c36df5120e888483f94cf
Ahmed Sheraz <ahmedsheraz1@gmail.com>
2022-12-14 01:14:24 +0500

    Create F-7PG Jet.py

---
+++ F-7PG Jet.py
@@ -0,0 +1,5 @@
+Python 3.10.2 (v3.10.2:a58ebcc701, Jan 13 2022, 14:50:16) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+from hashlib import blake2b
+blake2b(b'F-7PG Jet').hexdigest()
+'1b680c74291700f2aae63bd32a30a5d431479b35cd3803661d1d2d09ab9ad105d25809a881dbc74d6f30f82f4d1f491edaec946374052bf13a83815ddfda307c'
